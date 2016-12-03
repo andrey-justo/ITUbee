25d050710d3d1773e74d1d4b7fb714d63e9cf96a
andrey-justo-movile <andrey.justo@movile.com>
2016-12-02 23:20:17 -0200

    remove unecessary code

--- src/main.py
+++ src/main.py
@@ -3,25 +3,8 @@ Created on Nov 28, 2016
 
 @author: andrey.justo
 
-Some tips
-A|B : Concatenation of two bit strings A and B. K : 80-bit master key.
-KL : The left half of the master key.
-KR : The right half of the master key.
-P : 80-bit plaintext.
-PL : The left half of the plaintext.
-PR : The right half of the plaintext.
-C : 80-bit ciphertext.
-CL : The left half of the ciphertext.
-CR : The right half of the ciphertext.
-RCi : The round constant used in the i-th round.
-
-- import numpy as np
-- "7061756c".decode("hex")
 '''
-import operator
 
-blockSize = 80
-keySize = 80
 SBOX = [
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 
     0xFE, 0xD7, 0xAB, 0x76, 0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 
