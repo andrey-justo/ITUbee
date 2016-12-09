3043fa091514c3feae2e70af0fccda01767619bc
andrey-justo-movile <andrey.justo@movile.com>
2016-12-09 18:41:59 -0200

    new tests

--- src/tests.py
+++ src/tests.py
@@ -9,16 +9,60 @@ Tests for hexadecimal implementation
 from itubee.main import ITUbee
 import time
 
-start = time.time()
 itubee = ITUbee()
- 
-key = '00000000000000000080'
+
+# First test
 plain_text = '01000000000000000000'
+key = '00000000000000000080'
+
+start = time.time()
 enc = itubee.encrypt(plain_text, key)
-dec = itubee.decrypt(enc, key)
 print(enc)
+
+dec = itubee.decrypt(enc, key)
 print(dec)
-print(dec == plain_text)
+
+end = time.time()
+print(end - start)
+
+# 2 test
+plain_text = '00000000000000000000'
+key = '00000000000000000000'
+
+start = time.time()
+enc = itubee.encrypt(plain_text, key)
+print(enc)
+
+dec = itubee.decrypt(enc, key)
+print(dec)
+
+end = time.time()
+print(end - start)
+
+# 3 test
+plain_text = 'c538bd9289822be43363'
+key = '6925278951fbf3b25ccc'
+
+start = time.time()
+enc = itubee.encrypt(plain_text, key)
+print(enc)
+
+dec = itubee.decrypt(enc, key)
+print(dec)
+
+end = time.time()
+print(end - start)
+
+# 4 test
+plain_text = '6925288A51fCf3b25ccc'
+key = '6925288A51fCf3b25ccc'
+
+start = time.time()
+enc = itubee.encrypt(plain_text, key)
+print(enc)
+
+dec = itubee.decrypt(enc, key)
+print(dec)
 
 end = time.time()
 print(end - start)
